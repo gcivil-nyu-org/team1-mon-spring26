b332e2199eed8470fdde6b0fef6fe554b00e2d7e
Andrew Slezak <aslezak@Arielles-Mac-mini.local>
2026-02-26 21:47:51 -0600

    various ui performance improvements

--- package.sh
+++ package.sh
@@ -2,7 +2,8 @@ zip=../django_map.zip
 rm $zip
 
 # populate /static folder to be served by nginx
-echo yes|python3 manage.py collectstatic --clear
+####### we do this on elastic beanstalk, no need here! #######
+#echo yes|python3 manage.py collectstatic --clear
 
 # delete __pycache__ compiled bytecode files
 find . -name "__pycache__" -type d | xargs rm -rf
