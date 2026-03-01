63a8967e35cee3b203f2e773b70196b38b9c883c
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-03-01 21:33:20 +0000

    add load_data.sh and .devcontainer config for dev containers vs code

---
+++ load_data.sh
@@ -0,0 +1,5 @@
+python manage.py migrate
+python manage.py import_nyc_water_fountains
+python manage.py import_nyc_public_restrooms
+python manage.py import_cooling_sites
+python manage.py import_bike_racks
