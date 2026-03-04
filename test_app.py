af5adaa58b7e8770acda3868979c2134a2f31881
Andrew Slezak <aslezak@Arielles-Mac-mini.local>
2026-03-03 22:08:26 -0600

    fix black and flake issues

--- test_app.py
+++ test_app.py
@@ -1,13 +1,19 @@
 # test_app.py
 import sys
 
+
 def test_python_version():
     # Simple check to ensure we are running on Python 3
     assert sys.version_info.major == 3
 
+
 def test_import():
     try:
-        from maps.models import Amenity, AmenityType # Replace with your actual app/folder name
+        from maps.models import (
+            Amenity,
+            AmenityType,
+        )  # Replace with your actual app/folder name
+
         assert True
     except ImportError:
         assert False, "The application models could not be imported"
