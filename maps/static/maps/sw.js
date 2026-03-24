c073262da60bbce4256d29cfcbd6ad76778a932a
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-03-24 04:12:13 +0000

    update restroom icon, white wasnt visible on mobile

--- maps/static/maps/sw.js
+++ maps/static/maps/sw.js
@@ -1,13 +1,10 @@
-const CACHE_NAME = 'nyc-essentials-v1.04';
+const CACHE_NAME = 'nyc-essentials-v1.05';
 const ASSETS = [
     '/',
     '/static/maps/css/style.css',
     '/static/maps/js/map.js',
-    '/static/maps/favicon.ico',
-    '/static/maps/apple-touch-icon.png',
     '/static/maps/android-chrome-192x192.png',
-    '/static/maps/android-chrome-512x512.png',
-    '/static/maps/manifest.json'
+    '/static/maps/android-chrome-512x512.png'
 ];
 
 self.addEventListener('install', event => {
