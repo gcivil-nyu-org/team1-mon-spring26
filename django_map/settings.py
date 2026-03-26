2ebf1e450b1cad6e02136637a9a9b064c00fb022
ajslezak <andrew.j.slezak@gmail.com>
2026-03-26 00:02:01 -0500

    settings.py change

--- django_map/settings.py
+++ django_map/settings.py
@@ -208,7 +208,7 @@ else:
     }
 
     # Production: S3
-    #DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY"]
     # AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_KEY"]
