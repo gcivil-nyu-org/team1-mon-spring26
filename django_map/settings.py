4c2574e515bdcf856fd11f84fd71c34373eec3e0
ajslezak <andrew.j.slezak@gmail.com>
2026-03-26 01:09:11 -0500

    settings.py change

--- django_map/settings.py
+++ django_map/settings.py
@@ -201,9 +201,7 @@ else:
     STORAGES = {
         "default": {
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-            "OPTIONS": {
-                       "location": os.environ['APP_ENV']
-            },
+            "OPTIONS": {"location": os.environ["APP_ENV"]},
         },
         "staticfiles": {
             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
@@ -211,13 +209,13 @@ else:
     }
 
     # Production: S3
-    #DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY"]
     # AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_KEY"]
     AWS_QUERYSTRING_AUTH = True
     AWS_DEFAULT_ACL = None
     AWS_S3_SIGNATURE_VERSION = "s3v4"
-    #AWS_S3_CUSTOM_DOMAIN = f"{os.environ['AWS_S3_BUCKET_NAME']}.s3.{os.environ['AWS_S3_REGION_NAME']}.amazonaws.com"
+    # AWS_S3_CUSTOM_DOMAIN = f"{os.environ['AWS_S3_BUCKET_NAME']}.s3.{os.environ['AWS_S3_REGION_NAME']}.amazonaws.com"
 
-    #MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{APP_ENV}/"
+    # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{APP_ENV}/"
