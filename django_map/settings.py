3dc864f67dce5ffadd9b63113d13826f46dd6406
ajslezak <andrew.j.slezak@gmail.com>
2026-03-26 01:05:34 -0500

    settings.py change

--- django_map/settings.py
+++ django_map/settings.py
@@ -208,13 +208,13 @@ else:
     }
 
     # Production: S3
-    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    #DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY"]
     # AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_KEY"]
     AWS_QUERYSTRING_AUTH = True
     AWS_DEFAULT_ACL = None
     AWS_S3_SIGNATURE_VERSION = "s3v4"
-    AWS_S3_CUSTOM_DOMAIN = f"{os.environ['AWS_S3_BUCKET_NAME']}.s3.{os.environ['AWS_S3_REGION_NAME']}.amazonaws.com"
+    #AWS_S3_CUSTOM_DOMAIN = f"{os.environ['AWS_S3_BUCKET_NAME']}.s3.{os.environ['AWS_S3_REGION_NAME']}.amazonaws.com"
 
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{APP_ENV}/"
+    #MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{APP_ENV}/"
