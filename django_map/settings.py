63047a6374a45c32ed03f0057b4e8b726575d405
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-03-26 02:38:08 +0000

    remove s3 aws keys, use ec2 roles instead

--- django_map/settings.py
+++ django_map/settings.py
@@ -170,8 +170,8 @@ else:
     # Production: S3
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY"]
-    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_KEY"]
+    #AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY"]
+    #AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_KEY"]
     AWS_STORAGE_BUCKET = os.environ["AWS_S3_BUCKET_NAME"]
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-2")
     AWS_S3_SIGNATURE_VERSION = "s3v4"
