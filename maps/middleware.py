2d7412effe0ce16d5334da179a6cf3471284ed53
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-04-02 02:32:14 +0000

    fix flake8 and bk stupidity

--- maps/middleware.py
+++ maps/middleware.py
@@ -6,6 +6,7 @@ class SelectiveGZipMiddleware(GZipMiddleware):
     A custom GZip middleware that skips compression for Server-Sent Events (SSE)
     to prevent proxy and browser buffering issues.
     """
+
     def process_response(self, request, response):
         if "text/event-stream" in response.get("Content-Type", ""):
             return response
