3a0b62571805da8638a2a4b90f51e80ca697c0ad
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-04-02 02:02:19 +0000

    fix chat notifications!

--- django_map/settings.py
+++ django_map/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.gzip.GZipMiddleware",
 ]
 
 ROOT_URLCONF = "django_map.urls"
