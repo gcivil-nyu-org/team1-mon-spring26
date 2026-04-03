d6fb2ab1542e6312d90884457ab5ddaf35856f73
Andrew Slezak <38115854+ajslezak@users.noreply.github.com>
2026-04-03 14:38:00 -0600

    Remove post_fork function from gunicorn config
    Removed post_fork function for PostgreSQL gevent patching.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,12 +5,3 @@ worker_class = 'gevent'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 180  # High timeout for SSE persistent connections
 keepalive = 2  # between nginx and gunicorn
-
-def post_fork(server, worker):
-    # Required if using PostgreSQL with gevent
-    try:
-        from psycogreen.gevent import patch_psycopg
-        patch_psycopg()
-        server.log.info("Patched psycopg2 for gevent")
-    except ImportError:
-        pass
