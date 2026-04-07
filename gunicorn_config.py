9e7d763a06d7940465e1d1f27ed908da1a951d0f
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-04-07 02:50:18 +0000

    remove redundant configs for ebextensions

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,3 +5,6 @@ worker_class = "gevent"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 60
 keepalive = 2  # between nginx and gunicorn
+
+max_requests = 1000         # Restart workers after 1000 requests
+max_requests_jitter = 50    # Add randomness so all workers don't restart at the exact same time
