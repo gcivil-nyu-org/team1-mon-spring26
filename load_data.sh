e2babb4c5a2697f1a203a9a5c8bf89869a99c053
Andrew Slezak <andrew.j.slezak@gmail.com>
2026-04-11 14:14:26 +0000

    fix gsmtp

--- load_data.sh
+++ load_data.sh
@@ -7,6 +7,16 @@ echo "RDS_DB_HOST: $RDS_DB_HOST"
 echo "RDS_DB_PORT: $RDS_DB_PORT"
 
 python manage.py migrate
+
+# Dynamically update the default Django Site domain for emails (allauth)
+echo "Updating Django Site domain to match environment..."
+python manage.py shell -c "
+import os
+from django.contrib.sites.models import Site
+domain = os.environ.get('CF_RECORD_NAME', 'test.amenity.help')
+Site.objects.update_or_create(id=1, defaults={'domain': domain, 'name': 'NYC Now'})
+"
+
 python manage.py import_nyc_water_fountains
 python manage.py import_nyc_public_restrooms
 python manage.py import_cooling_sites
